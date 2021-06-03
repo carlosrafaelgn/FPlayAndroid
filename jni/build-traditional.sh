248e12a3752550c9ec7f6e858798bb44400f4885
Carlos Rafael Gimenes das Neves <carlosrafael.prog@gmail.com>
2021-06-03 08:27:59 -0300

    Creating bash scripts to build the native libraries in Linux

---
+++ jni/build-traditional.sh
@@ -0,0 +1,3 @@
+ndk-build NDK_APPLICATION_MK=./Application-traditional.mk clean
+ndk-build NDK_APPLICATION_MK=./Application-traditional.mk
+read -rsn1 -p"Press any key to continue";echo
---
+++ jni/build-x.sh
@@ -0,0 +1,3 @@
+ndk-build NDK_APPLICATION_MK=./Application-x.mk NDK_LIBS_OUT=../libs-x clean
+ndk-build NDK_APPLICATION_MK=./Application-x.mk NDK_LIBS_OUT=../libs-x
+read -rsn1 -p"Press any key to continue";echo
